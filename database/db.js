8be0ad02c18d499b98ee02c447da7e8a55469dec
LimonsitOwO <luislm080304@gmail.com>
2024-01-24 21:11:42 -0500

    Deploy 0.1

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
-const pool = new Pool({
+const db = new Pool({
   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 })
 export default db;

dee167dd01702cb61d8f36c9aa5bb5478d6d19ba
LimonsitOwO <luislm080304@gmail.com>
2024-01-24 20:37:49 -0500

    Deploy

--- app.js
+++ app.js
@@ -3,17 +3,11 @@ import cors from 'cors';
 import db from './database/db.js';
 import peliculaRoute from './routes/routes.js'
 
+require('dotenv').config()
 const app = express()
-
 const port = process.env.PORT || 8000;
 
 app.use(cors())
 app.use(express.json())
 app.use('/peliculas', peliculaRoute)
 
-try {
-    await db.authenticate()
-    console.log('Conexion exitosa a la DB')
-} catch (error) {
-    console.log(`Error de conexion: ${error}`)
-}

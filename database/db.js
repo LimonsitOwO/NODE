3fd6a753941a4145c98dc88b04447d5d67be2ec8
LimonsitOwO <luislm080304@gmail.com>
2024-02-01 15:00:29 -0500

    VD prueba

--- database/db.js
+++ database/db.js
@@ -1,3 +1,9 @@
+import { Sequelize } from "sequelize";
+const db = new Sequelize('postgres', 'postgres', '',{host:'localhost', dialect:'postgres'})
+
+export default db;
+
+/*
 import pg from 'pg';
 
 const { Pool } = pg;
@@ -5,4 +11,4 @@ const { Pool } = pg;
 const db = new Pool({
   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 })
-export default db;
+export default db;*/
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import db from './database/db.js';
+import pool from './database/db.js';
 import peliculaRoute from './routes/routes.js'
 
-require('dotenv').config()
+/*require('dotenv').config()*/
 const app = express()
 const port = process.env.PORT || 8000;
 
@@ -11,3 +11,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/peliculas', peliculaRoute)
 
+app.listen({port}, () => {
+    console.log(`Escuchando por le puerto ${port}: http://localhost:${port}/peliculas/`);
+})
